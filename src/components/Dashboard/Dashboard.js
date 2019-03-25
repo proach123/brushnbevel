779a5a0d6541473c47af6b71151a19ab37f8976b
Patrick Roach <patrickevanroach@gmail.com>
2019-03-25 10:34:18 -0600

    fixed bug

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import '../../style/dashboard.css'
+import '../../style/dashboard.scss'
 import { Link } from 'react-router-dom'
 
 class Dashboard extends Component {
