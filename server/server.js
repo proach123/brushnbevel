ae0256363d53f2cc1ef26c8087e507263afd7578
Patrick Roach <patrickevanroach@gmail.com>
2019-03-25 10:15:23 -0600

    hunter

--- server/server.js
+++ server/server.js
@@ -15,6 +15,7 @@ const express = require('express'),
     })
     
     app.use( express.static( `${__dirname}/../build` ) );
+    
     app.use(express.json());
     app.use(sessions({
         store: new pgSession({
